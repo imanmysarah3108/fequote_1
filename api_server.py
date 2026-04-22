6000b2cd7755113dcc09e2ed3d692352afb3fc41
imanmysarah3108 <2023216898@student.uitm.edu.my>
2026-04-22 20:54:35 +0800

    Fix API startup crash

--- api_server.py
+++ api_server.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-import shutil
 import os
 from recommender_system.main_pipeline import recommend_quotes
 from dataset.fer_gemini import detect_emotion
@@ -20,12 +19,26 @@ UPLOAD_FOLDER = "temp"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 
+@app.get("/")
+def root():
+    return {"message": "API is running"}   # health check
+
+
 @app.post("/analyze")
 async def analyze(image: UploadFile = File(...)):
-    emotion = detect_emotion(image)
-    quotes = recommend_quotes(emotion)
+    try:
+        file_path = os.path.join(UPLOAD_FOLDER, image.filename)
 
-    return {
-        "emotion": emotion,
-        "quotes": quotes
-    }
+        with open(file_path, "wb") as buffer:
+            buffer.write(await image.read())
+
+        emotion = detect_emotion(file_path)
+        quotes = recommend_quotes(emotion)
+
+        return {
+            "emotion": emotion,
+            "quotes": quotes
+        }
+
+    except Exception as e:
+        return {"error": str(e)}
