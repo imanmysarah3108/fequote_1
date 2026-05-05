d6d910e440f8c8bab19e13d67713b5bbd6240956
imanmysarah3108 <2023216898@student.uitm.edu.my>
2026-05-05 15:39:34 +0800

    Firebase connection + Android SDK troubleshoot

--- mobile_app/flutter_app/lib/main.dart
+++ mobile_app/flutter_app/lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'constants/app_theme.dart';
 import 'screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const FaceQuoteApp());
 }
 
--- mobile_app/flutter_app/lib/services/api_service.dart
+++ mobile_app/flutter_app/lib/services/api_service.dart
@@ -4,35 +4,38 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   static Future<Map<String, dynamic>> detectEmotion(File imageFile) async {
-  try {
-    var request = http.MultipartRequest(
-      'POST',
-      Uri.parse('https://fequote-api-155804644015.asia-southeast1.run.app/analyze'),
-    );
+    try {
+      print("🚀 Sending request to API...");
+      print("📸 Image path: ${imageFile.path}");
 
-    request.files.add(
-      await http.MultipartFile.fromPath('image', imageFile.path),
-    );
+      var request = http.MultipartRequest(
+        'POST',
+        Uri.parse('https://fequote-api-155804644015.asia-southeast1.run.app/analyze'),
+      );
 
-    var response = await request.send();
+      request.files.add(
+        await http.MultipartFile.fromPath('image', imageFile.path),
+      );
 
-    var responseData = await response.stream.bytesToString();
+      var response = await request.send();
+      var responseData = await response.stream.bytesToString();
 
-    if (response.statusCode == 200) {
-      var jsonData = jsonDecode(responseData);
+      print("📡 Status Code: ${response.statusCode}");
+      print("📦 Response Body: $responseData");
 
-      return {
-        "emotion": jsonData['emotion'],
-        "quotes": List<String>.from(jsonData['quotes']),
-      };
-    } else {
-      throw Exception("Server error");
+      if (response.statusCode == 200) {
+        var jsonData = jsonDecode(responseData);
+
+        return {
+          "emotion": jsonData["emotion"],
+          "quotes": List<String>.from(jsonData["quotes"]),
+        };
+      } else {
+        throw Exception("Server error: $responseData");
+      }
+    } catch (e) {
+      print("❌ Exception: $e");
+      rethrow;
     }
-  } catch (e) {
-    print("❌ $e");
-    return {
-      "emotion": "error",
-      "quotes": []
-    };
   }
-}}
+}
